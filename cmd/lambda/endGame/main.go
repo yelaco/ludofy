25e3639d9b427d591b1c9568f3660e1ed061632c
Quang Bui M. <minhquangbui053@gmail.com>
2025-04-25 15:08:02 +0700

    Update end game lambda

--- cmd/lambda/endGame/main.go
+++ cmd/lambda/endGame/main.go
@@ -23,7 +23,7 @@ func init() {
 func handler(ctx context.Context, event json.RawMessage) error {
 	var matchRecordReq server.MatchRecordRequest
 	if err := json.Unmarshal(event, &matchRecordReq); err != nil {
-		return fmt.Errorf("failed to unmarshal request: %w", err)
+		return fmt.Errorf("failed to unmarshal: %w", err)
 	}
 	matchRecord := server.MatchRecordRequestToEntity(matchRecordReq)
 
