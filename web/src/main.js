33f4eba9f8f31a66682f5d1fd301a9bcd1072835
Quang Bui M. <minhquangbui053@gmail.com>
2025-04-27 16:15:50 +0700

    Fix deploying

--- web/src/main.js
+++ web/src/main.js
@@ -5,8 +5,12 @@ import App from "./App.vue";
 import router from "./router";
 import { handleAuthRedirect } from "@/auth"; // ⬅️ import the handler
 
-await handleAuthRedirect(); // ⬅️ Important! complete login before mounting
+async function main() {
+  await handleAuthRedirect(); // ⬅️ Important! complete login before mounting
 
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
+  const app = createApp(App);
+  app.use(router);
+  app.mount("#app");
+}
+
+main();
